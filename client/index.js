061b6cb60ecf4e59ddd3c2c8ecd9e02c324f67e7
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-10-13 19:26:39 +1100

    move left right cases

--- client/index.js
+++ client/index.js
@@ -1,30 +1,33 @@
 console.log("hello")
 const positionDirection = {
     NORTH : {
-        LEFT: "west",
-        RIGHT:"east"
+        LEFT: "WEST",
+        RIGHT: "EAST"
     },
     SOUTH : {
-        LEFT: "east" ,
-        RIGHT: "west"
+        LEFT: "EAST" ,
+        RIGHT: "WEST"
     },
     EAST : {
-        LEFT: "north" ,
-        RIGHT: "south"
+        LEFT: "NORTH" ,
+        RIGHT: "SOUTH"
     },
     WEST : {
-        LEFT: "south" ,
-        RIGHT:  "north"
+        LEFT: "SOUTH" ,
+        RIGHT:  "NORTH"
     }
 }
 
 const userInput =  document.getElementById('input-textbox')
 console.log(userInput,"here")
-userInput.addEventListener('change', roboCommand);
+
+userInput.addEventListener('submit', roboCommand);
 let  roboPosition =  {}
 
 function roboCommand(e) {
-  const userInputRobo = e.target.value
+// const userInput =  document.getElementById('input-textbox').value
+console.log(userInput,"userInput")
+  const userInputRobo = document.getElementById('input-textbox').value
 
   console.log(userInputRobo,"robo position")
   const userInputArray = userInputRobo.split(' ')
@@ -58,20 +61,41 @@ function roboCommand(e) {
     roboPosition = {...roboPosition,Facing:newfacing}
     console.log(roboPosition,"Roboposition")
  }
-//  if(userInputArray[0].toUpperCase() === "RIGHT" ){
-//     console.log(roboPosition, "roboPosition") 
-//     const userInputFacingArray = userInputArray[0].split(",")
-//     const movement = userInputFacingArray[0].toUpperCase()
-//     console.log(movement,"robo details")
-//     roboOldFacing = roboPosition["Facing"]
-//     console.log(roboOldFacing,"roboOldFacing") 
-//     console.log(positionDirection[roboPosition["Facing"]][movement],"positionDirection[roboOldFacing][movement]")
-//     let newfacing = positionDirection[roboOldFacing][movement]
-//     console.log(newfacing,"newfacing")
-//     roboPosition = {...roboPosition,Facing:newfacing}
-//     console.log(roboPosition,"Roboposition")
-//  }
 
+ if(userInputArray[0].toUpperCase() === "RIGHT" ){
+    console.log(roboPosition, "roboPosition") 
+    
+    const movement = userInputArray[0].toUpperCase()
+    console.log(movement,"robo details")
+    roboOldFacing = roboPosition["Facing"]
+    console.log(roboOldFacing,"roboOldFacing") 
+   
+    console.log(positionDirection[roboPosition["Facing"]][movement],"positionDirection[roboOldFacing][movement]")
+    let newfacing = positionDirection[roboOldFacing][movement]
+    console.log(newfacing,"newfacing")
+    roboPosition = {...roboPosition,Facing:newfacing}
+    console.log(roboPosition,"Roboposition")
+ }
+
+ if(userInputArray[0].toUpperCase() === "MOVE" &&(roboPosition["Facing"]== "EAST" || "WEST")){
+
+    console.log(" in move ")
+    let oldvalue = Number(roboPosition.X) + 1
+    console.log(oldvalue)
+    roboPosition = {
+            ...roboPosition,
+            X: oldvalue
+        }
+        console.log(roboPosition)
+ }
+//  if(userInputArray[0].toUpperCase() == "MOVE" &&(roboPosition[Facing]== "EAST" || "WEST")){
+    
+//     roboPosition = {
+//         ...roboPosition,
+//         Y: roboPosition[Y]+1
+//     }
+//     console.log(roboPosition,"line89")
+//  }
 
 
 }
