566943057e4677ca5d2cf42e94bb586937bc9e38
Ankita Sawrav <ankita.sawrav@gmail.com>
2022-10-14 00:03:25 +1100

    refactoring turnRight and turnLeft code

--- client/index.js
+++ client/index.js
@@ -48,97 +48,19 @@ function roboCommand(userInputRobo,roboPosition) {
 
 
     if (userInputArray[0].toUpperCase() === "PLACE") {
-        const userInputFacingArray = userInputArray[1].split(",")
-
-        const x = userInputFacingArray[0]
-        const y = userInputFacingArray[1]
-        const facing = userInputFacingArray[2].toUpperCase()
-        console.log(x, y, facing, "robo details")
-        roboPosition = {
-            X: x,
-            Y: y,
-            Facing: facing
-        }
-        console.log(roboPosition, "roboPosition")
+        roboPosition = Place(userInputArray, roboPosition)
     }
     if (userInputArray[0].toUpperCase() === "LEFT") {
-        console.log("in line 52 left ")
-        console.log(roboPosition, "roboPosition")
-        const userInputFacingArray = userInputArray[0].split(",")
-        const movement = userInputFacingArray[0].toUpperCase()
-        console.log(movement, "robo details")
-        roboOldFacing = roboPosition["Facing"]
-        console.log(roboOldFacing, "roboOldFacing")
-        console.log(positionDirection[roboOldFacing][movement], "positionDirection[roboOldFacing][movement]")
-        let newfacing = positionDirection[roboOldFacing][movement]
-        console.log(newfacing, "newfacing")
-        roboPosition = {
-            ...roboPosition,
-            Facing: newfacing
-        }
-        console.log(roboPosition, "Roboposition")
+        roboPosition = turnLeft(roboPosition)
     }
 
     if (userInputArray[0].toUpperCase() === "RIGHT") {
 
-        console.log(roboPosition, "roboPosition in line 68 right")
-
-        const movement = userInputArray[0].toUpperCase()
-        console.log(movement, "robo details")
-        roboOldFacing = roboPosition["Facing"]
-        console.log(roboOldFacing, "roboOldFacing")
-
-        console.log(positionDirection[roboPosition["Facing"]][movement], "positionDirection[roboOldFacing][movement]")
-        let newfacing = positionDirection[roboOldFacing][movement]
-        console.log(newfacing, "newfacing")
-        roboPosition = {
-            ...roboPosition,
-            Facing: newfacing
-        }
-        console.log(roboPosition, "Roboposition")
+        roboPosition = turnRight(roboPosition)
     }
 
     if (userInputArray[0].toUpperCase() === "MOVE") {
-        if (roboPosition["Facing"] == "EAST" && roboPosition.X < 5) {
-            console.log(" in EAST ")
-            let newvalueX = Number(roboPosition.X) + 1
-            console.log(newvalueX)
-            roboPosition = {
-                ...roboPosition,
-                X: newvalueX
-            }
-            console.log(roboPosition)
-        }
-        if (roboPosition["Facing"] == "WEST" && roboPosition.X > 0) {
-            console.log(" in West ")
-            let newvalueX = Number(roboPosition.X) - 1
-            console.log(newvalueX)
-            roboPosition = {
-                ...roboPosition,
-                X: newvalueX
-            }
-            console.log(roboPosition)
-        }
-        if (roboPosition["Facing"] == "NORTH" && roboPosition.Y < 5) {
-            console.log(" in North ")
-            let newvalueY = Number(roboPosition.Y) + 1
-            console.log(newvalueY)
-            roboPosition = {
-                ...roboPosition,
-                Y: newvalueY
-            }
-            console.log(roboPosition)
-        }
-        if (roboPosition["Facing"] == "SOUTH" && roboPosition.Y > 0) {
-            console.log(" in South ")
-            let newValueY = Number(roboPosition.Y) - 1
-            console.log(newValueY)
-            roboPosition = {
-                ...roboPosition,
-                Y: newValueY
-            }
-            console.log(roboPosition)
-        }
+        roboPosition = Move(roboPosition)
 
     }
     if(userInputArray[0].toUpperCase() === "REPORT"){
@@ -146,3 +68,95 @@ function roboCommand(userInputRobo,roboPosition) {
     }
     return roboPosition
 }
+function Place(userInputArray, roboPosition) {
+    const userInputFacingArray = userInputArray[1].split(",")
+
+    const x = userInputFacingArray[0]
+    const y = userInputFacingArray[1]
+    const facing = userInputFacingArray[2].toUpperCase()
+    console.log(x, y, facing, "robo details")
+    roboPosition = {
+        X: x,
+        Y: y,
+        Facing: facing
+    }
+    console.log(roboPosition, "roboPosition")
+    return roboPosition
+}
+
+function Move(roboPosition) {
+    if (roboPosition["Facing"] == "EAST" && roboPosition.X < 5) {
+        console.log(" in EAST ")
+        let newvalueX = Number(roboPosition.X) + 1
+        console.log(newvalueX)
+        roboPosition = {
+            ...roboPosition,
+            X: newvalueX
+        }
+        console.log(roboPosition)
+    }
+    if (roboPosition["Facing"] == "WEST" && roboPosition.X > 0) {
+        console.log(" in West ")
+        let newvalueX = Number(roboPosition.X) - 1
+        console.log(newvalueX)
+        roboPosition = {
+            ...roboPosition,
+            X: newvalueX
+        }
+        console.log(roboPosition)
+    }
+    if (roboPosition["Facing"] == "NORTH" && roboPosition.Y < 5) {
+        console.log(" in North ")
+        let newvalueY = Number(roboPosition.Y) + 1
+        console.log(newvalueY)
+        roboPosition = {
+            ...roboPosition,
+            Y: newvalueY
+        }
+        console.log(roboPosition)
+    }
+    if (roboPosition["Facing"] == "SOUTH" && roboPosition.Y > 0) {
+        console.log(" in South ")
+        let newValueY = Number(roboPosition.Y) - 1
+        console.log(newValueY)
+        roboPosition = {
+            ...roboPosition,
+            Y: newValueY
+        }
+        console.log(roboPosition)
+    }
+    return roboPosition
+}
+
+function turnRight(roboPosition) {
+    const movement = "RIGHT"
+    roboOldFacing = roboPosition["Facing"]
+    console.log(roboOldFacing, "roboOldFacing")
+
+    console.log(positionDirection[roboPosition["Facing"]][movement], "positionDirection[roboOldFacing][movement]")
+    let newfacing = positionDirection[roboOldFacing][movement]
+    console.log(newfacing, "newfacing")
+    roboPosition = {
+        ...roboPosition,
+        Facing: newfacing
+    }
+    console.log(roboPosition, "Roboposition")
+    return roboPosition
+}
+
+function turnLeft(roboPosition) {
+    const movement = "LEFT"
+    console.log(movement, "robo details")
+    roboOldFacing = roboPosition["Facing"]
+    console.log(roboOldFacing, "roboOldFacing")
+    console.log(positionDirection[roboOldFacing][movement], "positionDirection[roboOldFacing][movement]")
+    let newfacing = positionDirection[roboOldFacing][movement]
+    console.log(newfacing, "newfacing")
+    roboPosition = {
+        ...roboPosition,
+        Facing: newfacing
+    }
+    console.log(roboPosition, "Roboposition")
+    return roboPosition
+}
+
